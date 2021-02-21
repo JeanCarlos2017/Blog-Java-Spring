4b00f66e897195230cc7be0cb700fd275b7ffa87
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-02-20 21:06:20 -0300

    Update BlogPessoalApplicationTests.java
    criando o primeiro teste de sucesso no Spring

--- src/test/java/com/blogpessoal/BlogPessoalApplicationTests.java
+++ src/test/java/com/blogpessoal/BlogPessoalApplicationTests.java
@@ -1,13 +1,21 @@
 package com.blogpessoal;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.blogpessoal.api.controller.IndexController;
 
 @SpringBootTest
 class BlogPessoalApplicationTests {
-
+	
+	@Autowired
+	private IndexController index;
 	@Test
 	void contextLoads() {
+		assertThat(index).isNotNull();
 	}
 
 }
