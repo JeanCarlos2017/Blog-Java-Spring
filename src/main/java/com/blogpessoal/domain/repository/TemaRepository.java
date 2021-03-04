dcb9e1c66c6f339503cda88c2017d3e7d73a5699
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-03-04 11:25:23 -0300

    Update TemaRepository.java

--- src/main/java/com/blogpessoal/domain/repository/TemaRepository.java
+++ src/main/java/com/blogpessoal/domain/repository/TemaRepository.java
@@ -3,9 +3,11 @@ package com.blogpessoal.domain.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.blogpessoal.domain.model.Tema;
 
+@Repository
 public interface TemaRepository extends JpaRepository<Tema, Long>{
 	public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao); 
 	public Tema findByDescricao(String descricao);
