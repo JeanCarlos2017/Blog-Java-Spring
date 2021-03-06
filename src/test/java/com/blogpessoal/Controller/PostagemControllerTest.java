bfadee0ef2b1ba87a303cbf10073208d5c2485ee
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-03-05 22:36:57 -0300

    Update PostagemControllerTest.java
    teste: findAll, getById, GetByTitulo

--- src/test/java/com/blogpessoal/Controller/PostagemControllerTest.java
+++ src/test/java/com/blogpessoal/Controller/PostagemControllerTest.java
@@ -1,12 +1,14 @@
 package com.blogpessoal.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -29,17 +31,21 @@ public class PostagemControllerTest {
 	
 	@BeforeEach
 	void setUp() {
-		List<Postagem> postList= List.of(PostagemCreator.criaPostagem());
 		//quando chamar o postService.getPostagemRepository() retorna o repositório mockado
 		BDDMockito.when(postService.getPostagemRepository()).thenReturn(postRepository);
-		//quando chamar o medodo findAll vai retornar o postagemPage
-		BDDMockito.when(postService.getPostagemRepository().findAll())
-					.thenReturn(postList);
+		
 	}
 	
 	@Test
 	@DisplayName("Retorna lista de postagens dentro po postList")
 	void testFindAll_Sucessful() {
+		//criando uma lista de postagem com um elemento
+		List<Postagem> postList= List.of(PostagemCreator.criaPostagem());
+		
+		//quando chamar o medodo findAll vai retornar o postagemPage
+				BDDMockito.when(postService.getPostagemRepository().findAll())
+							.thenReturn(postList);
+		//objetos que vamos comparar 
 		String tituloEsperado= PostagemCreator.criaPostagem().getTitulo();
 		List<Postagem> respostaFindAll= postagemController.findAll().getBody();
 		//realizando os testes 
@@ -50,6 +56,31 @@ public class PostagemControllerTest {
 		Assertions.assertThat(respostaFindAll.get(0).getTitulo())
 								.isEqualTo(tituloEsperado);
 		
+	}
+	
+	@Test
+	@DisplayName("busca por id Sucesso")
+	void findById_Sucessful() {
+		//quando chamar o medodo findAll vai retornar o postagemPage
+		BDDMockito.when(postService.getPostagemRepository().findById(ArgumentMatchers.anyLong()))
+										.thenReturn(Optional.of(PostagemCreator.criaPostagem_Save()));
+		Optional<Postagem> postFindById= postService.getPostagemRepository().findById(321L);
+		Assertions.assertThat(postFindById).isPresent();
+		Assertions.assertThat(postFindById.get().getTitulo())
+					.isEqualTo(PostagemCreator.criaPostagem_Save().getTitulo());
+	}
+	
+	@Test
+	@DisplayName("Busca por titulo: Sucesso")
+	void findByTitulo() {
+		// criando uma lista de postagem com um elemento
+		List<Postagem> postList = List.of(PostagemCreator.criaPostagem());
+		BDDMockito.when(
+				postService.getPostagemRepository().findAllByTituloContainingIgnoreCase(ArgumentMatchers.anyString()))
+				.thenReturn(postList);
+		// realizando os testes
+		Assertions.assertThat(postList).isNotNull().isNotEmpty().hasSize(1);
+		Assertions.assertThat(postList.get(0).getTitulo()).isEqualTo(PostagemCreator.criaPostagem().getTitulo());
 	}
 
 }
