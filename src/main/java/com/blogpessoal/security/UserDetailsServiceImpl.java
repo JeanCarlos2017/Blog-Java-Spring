62f68c60b1e21f45595425fe567c59dcc836f092
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-03-16 13:47:40 -0300

    security

---
+++ src/main/java/com/blogpessoal/security/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.blogpessoal.security;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.blogpessoal.domain.model.UsuarioEntidade;
+import com.blogpessoal.domain.repository.UsuarioRepository;
+
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException{
+		Optional<UsuarioEntidade> user= usuarioRepository.findByNome(userName);
+		user.orElseThrow(()-> new UsernameNotFoundException(userName + "not found"));
+		return user.map(UserDetailsImpl::new).get();
+	}
+
+}
